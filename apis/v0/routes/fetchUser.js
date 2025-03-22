96cfbe246e84779fbdaf737044a4008827d23e2c
Emilia Luminé <eqilia@national.shitposting.agency>
2025-03-22 22:34:48 +0100

    feat: api: Add fetchUser (data/user)
    O_O
    im surprised its even going somewhere lmfao
    
    Signed-off-by: Emilia Luminé <eqilia@national.shitposting.agency>

---
+++ apis/v0/routes/fetchUser.js
@@ -0,0 +1,31 @@
+const { getUserData } = require("../../../intlibs/common.js");
+
+module.exports = {
+	method: "get",
+	path: "data/user",
+	async execute(req, res, next) {
+		if (typeof req.body?.userId !== 'string') {
+			res.status(403);
+			res.json({ success: false, code: -1 });
+			return;
+		}
+
+		const userData = await getUserData(userId);
+		
+		if (userData === null) {
+			res.status(403);
+			res.json({ success: false, code: -2 });
+			return;
+		}
+
+		res.json({
+			success: true,
+			code: 0,
+			payload: {
+				date: Date.now()
+			}
+		});
+
+		return;
+	}
+}
